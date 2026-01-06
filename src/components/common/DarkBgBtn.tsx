827b134811b7aab72ba49b5553968ff73b41f91f
himanshuhestabit <himanshup.hestabit@gmail.com>
2026-01-06 17:58:49 +0530

    revert dark btn

--- src/components/common/DarkBgBtn.tsx
+++ src/components/common/DarkBgBtn.tsx
@@ -1,26 +1,19 @@
 import Link from "next/link";
-interface DarkBgBtnProps {
-  href: string;
-  children: React.ReactNode;
-  onClick?: () => void;
-}
 
-const DarkBgBtn = ({ href, children, onClick = () => {} }: DarkBgBtnProps) => {
-  const buttonCLass =
-    "body-sm-600 flex h-12 items-center justify-center rounded-md bg-[rgb(var(--primary-500))] px-6 text-[rgb(var(--white))] transition-colors hover:bg-[rgb(var(--primary-600))]";
-  if (onClick === undefined) {
-    return (
-      <Link href={href} className={buttonCLass}>
-        {children}
-      </Link>
-    );
-  } else {
-    return (
-      <button className={buttonCLass} onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
+const DarkBgBtn = ({
+  href = "/sign-up",
+  children = "Create Account",
+  onClick = () => {},
+}) => {
+  return (
+    <Link
+      href={href}
+      className="body-sm-600 flex h-12 items-center justify-center rounded-md bg-[rgb(var(--primary-500))] px-6 text-[rgb(var(--white))] transition-colors hover:bg-[rgb(var(--primary-600))]"
+      onClick={onClick}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default DarkBgBtn;
