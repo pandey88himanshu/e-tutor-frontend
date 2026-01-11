c968bc5b45d5ee765117c5b69dd0dfbbc759089e
pandey88himanshu <pandey88himanshu@gmail.com>
2026-01-11 22:52:36 +0530

    working on instructor section

--- src/components/common/TopNavbar.tsx
+++ src/components/common/TopNavbar.tsx
@@ -2,33 +2,34 @@
 
 import Link from "next/link";
 
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "Courses", href: "/courses" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+  { name: "Become an Instructor", href: "/become-instructor" },
+] as const;
+
 export default function TopNavbar() {
   return (
-    <div className="bg-[rgb(var(--gray-900))]">
-      <div className="mx-auto max-w-7xl px-6">
-        <div className="flex h-10 items-center justify-between">
+    <div className='bg-[rgb(var(--gray-900))]'>
+      <div className='mx-auto max-w-7xl px-6'>
+        <div className='flex h-10 items-center justify-between'>
           {/* LEFT LINKS */}
-          <div className="hidden md:flex items-center gap-6">
-            {[
-              "Home",
-              "Courses",
-              "About",
-              "Contact",
-              "Become an Instructor",
-            ].map((item) => (
+          <div className='hidden md:flex items-center gap-6'>
+            {navItems.map((item) => (
               <Link
-                key={item}
-                href="#"
-                className="body-xs-400 text-[rgb(var(--gray-300))] hover:text-[rgb(var(--white))]"
-              >
-                {item}
+                key={item.name}
+                href={item.href}
+                className='body-xs-400 text-[rgb(var(--gray-300))] hover:text-[rgb(var(--white))]'>
+                {item.name}
               </Link>
             ))}
           </div>
 
           {/* RIGHT OPTIONS */}
-          <div className="flex items-center gap-4">
-            <button className="body-xs-400 text-[rgb(var(--gray-300))] hover:text-[rgb(var(--white))]">
+          <div className='flex items-center gap-4'>
+            <button className='body-xs-400 text-[rgb(var(--gray-300))] hover:text-[rgb(var(--white))]'>
               Contact Us
             </button>
           </div>
